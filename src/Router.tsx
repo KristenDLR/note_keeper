8b79c79bad98b1365bcdc5ea724229bc9a94bfb7
Kristen De La Rosa <delarosa.kristen@gmail.com>
2025-01-24 15:52:27 -0800

    feat: skeleton of new note page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,28 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { HomePage } from './pages/Home.page';
+import { NewNote } from './pages/NewNote';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
+  },
+  {
+    path: '/new',
+    element: <NewNote />,
+  },
+  {
+    path: '/:id',
+    children: [
+      {
+        index: true,
+        element: <h1>Show</h1>,
+      },
+      {
+        path: 'edit',
+        element: <h1>Edit</h1>,
+      }
+    ],
   },
 ]);
 
